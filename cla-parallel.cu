ed40ff8fedb25dc25613b5c7592df72e4d86ba13
matthewcrotty <mjc2011@verizon.net>
2022-02-22 09:59:34 -0500

    asldkjfa

--- cla-parallel.cu
+++ cla-parallel.cu
@@ -485,17 +485,23 @@ void compute_group_carry()
 /***********************************************************************************************************/
 __global__
 void compute_carry_c(int* ci_c, int* gi_c, int* pi_c, int* gcj_c){
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if(index == 0){
+        ci_c[0] = gi_c[0] | 0;
+    }
     if(threadIdx.x < ngroups){
-        int index = blockIdx.x * blockDim.x + threadIdx.x;
-        ci_c[index * block_size] = gi_c[index * block_size];
+        if(index != 0)
+            ci_c[index * block_size] = gi_c[index * block_size] | (pi_c[index * block_size] & gcj_c[index-1]);
+        __syncthreads();
         index *= block_size;
-        for(int i = 1; i < block_size -1; i++){
+        for(int i = 1; i < block_size; i++){
             if(index + i < bits){
                 ci_c[index + i] = gi_c[index + i] | (pi_c[index + i] & ci_c[index+i -1]);
             }
         }
-        index += block_size;
-        ci_c[index * block_size] = gi_c[index * block_size] | (pi_c[index * block_size] & gcj_c[index]);
+
+        // index += block_size;
+        //
     }
 }
 
